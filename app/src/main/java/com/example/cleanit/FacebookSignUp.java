0de2e3030fd3476ecaf0a434368bae13113ed985
Adel Araji <adel_araji@hotmail.com>
2020-03-28 17:39:06 -0400

    Additional Information Page

--- app/src/main/java/com/example/cleanit/FacebookSignUp.java
+++ app/src/main/java/com/example/cleanit/FacebookSignUp.java
@@ -2,7 +2,9 @@ package com.example.cleanit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FacebookSignUp extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class FacebookSignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facebook_sign_up);
+    }
+
+    public void goToAdditionalSignUpPage(View view) {
+        Intent intent = new Intent(this, AdditionalSignUpStage.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/cleanit/GoogleSignUp.java
+++ app/src/main/java/com/example/cleanit/GoogleSignUp.java
@@ -2,7 +2,9 @@ package com.example.cleanit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class GoogleSignUp extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class GoogleSignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_sign_up);
+    }
+
+    public void goToAdditionalSignUpPage(View view) {
+        Intent intent = new Intent(this, AdditionalSignUpStage.class);
+        startActivity(intent);
     }
 }
