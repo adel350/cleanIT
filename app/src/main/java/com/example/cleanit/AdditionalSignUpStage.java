313549697b7e2cf2f0a14fffa63d1f8397db6d92
Adel Araji <adel_araji@hotmail.com>
2020-03-28 18:11:04 -0400

    Payment Information Screen

--- app/src/main/java/com/example/cleanit/AdditionalSignUpStage.java
+++ app/src/main/java/com/example/cleanit/AdditionalSignUpStage.java
@@ -2,7 +2,9 @@ package com.example.cleanit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AdditionalSignUpStage extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class AdditionalSignUpStage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_additional_sign_up_stage);
+    }
+
+    public void goToPaymentInformation(View view) {
+        Intent intent = new Intent(this, PaymentInformation.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/cleanit/PaymentInformation.java
@@ -0,0 +1,14 @@
+package com.example.cleanit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PaymentInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_information);
+    }
+}
