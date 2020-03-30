703513f55493e42f146ce46154ed65e30c66f33e
Adel Araji <adel_araji@hotmail.com>
2020-03-30 00:59:49 -0400

    Populated spinners in Payment Info page

--- app/src/main/java/com/example/cleanit/PaymentInformation.java
+++ app/src/main/java/com/example/cleanit/PaymentInformation.java
@@ -5,13 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class PaymentInformation extends AppCompatActivity {
+
+    private Spinner spinner1;
+    private ArrayAdapter<CharSequence> spinner_adapter1;
+
+    private Spinner spinner2;
+    private ArrayAdapter<CharSequence> spinner_adapter2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_information);
+
+        spinner1 = findViewById(R.id.update_month_expiration);
+        spinner_adapter1 = ArrayAdapter.createFromResource(this, R.array.month, android.R.layout.simple_spinner_item);
+        spinner_adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(spinner_adapter1);
+
+        spinner2 = findViewById(R.id.update_year_expiration);
+        spinner_adapter2 = ArrayAdapter.createFromResource(this, R.array.year, android.R.layout.simple_spinner_item);
+        spinner_adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(spinner_adapter2);
     }
 
     public void addPaymentInformation(View view) {
