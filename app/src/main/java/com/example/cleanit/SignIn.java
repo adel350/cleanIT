daa3bc3644cb8941d5f43b9b8ff39e985c222db8
Adel Araji <adel_araji@hotmail.com>
2020-04-03 14:38:53 -0400

    Added error checking for sign in page

--- app/src/main/java/com/example/cleanit/SignIn.java
+++ app/src/main/java/com/example/cleanit/SignIn.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 
 public class SignIn extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +20,36 @@ public class SignIn extends AppCompatActivity {
     }
 
     public void goToAccountDetails(View view) {
-        Intent intent = new Intent(this, AccountDetails.class);
-        startActivity(intent);
+        //Get email and password before clickinng the button
+        emailEditText = findViewById(R.id.emailInputText);
+        passwordEditText = findViewById(R.id.passwordInputText);
+
+        if (validForm()) {
+            Intent intent = new Intent(this, AccountDetails.class);
+            startActivity(intent);
+        }
+
+    }
+
+
+    public boolean validForm() {
+        boolean error = true;
+
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+
+        if (TextUtils.isEmpty(email)) {
+            emailEditText.setError("Email cannot be empty");
+            error = false;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            passwordEditText.setError("Password cannot be empty");
+            error = false;
+        }
+
+        return error;
     }
 
 }
