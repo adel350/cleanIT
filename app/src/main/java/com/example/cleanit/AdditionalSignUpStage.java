f4d44d5b84da4806be9e51e38b75cfc9f6683ca4
Adel Araji <adel_araji@hotmail.com>
2020-04-03 14:53:43 -0400

    Added error checking for Additional Information page

--- app/src/main/java/com/example/cleanit/AdditionalSignUpStage.java
+++ app/src/main/java/com/example/cleanit/AdditionalSignUpStage.java
@@ -4,9 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 
 public class AdditionalSignUpStage extends AppCompatActivity {
+
+    private EditText phoneNumberEditText;
+    private EditText streetAddressEditText;
+    private EditText apartmentNumberEditText;
+    private EditText cityEditText;
+    private EditText postalCodeEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +28,55 @@ public class AdditionalSignUpStage extends AppCompatActivity {
     }
 
     public void goToAccountDetails(View view) {
-        Intent intent = new Intent(this, AccountDetails.class);
-        startActivity(intent);
+
+        //Get info before going to the next page
+        phoneNumberEditText = findViewById(R.id.phoneNumberInput);
+        streetAddressEditText = findViewById(R.id.streetAddressInput);;
+        apartmentNumberEditText = findViewById(R.id.apartmentNumberInput);
+        cityEditText = findViewById(R.id.cityInput);
+        postalCodeEditText = findViewById(R.id.postalCodeInput);
+
+        if (validForm()) {
+            Intent intent = new Intent(this, AccountDetails.class);
+            startActivity(intent);
+        }
+    }
+
+    public boolean validForm() {
+        boolean error = true;
+
+        String phoneNumber = phoneNumberEditText.getText().toString().trim();
+        String streetAddress = streetAddressEditText.getText().toString().trim();
+        String apartmentNumber = apartmentNumberEditText.getText().toString().trim();
+        String city = cityEditText.getText().toString().trim();
+        String postalCode = postalCodeEditText.getText().toString().trim();
+
+
+        if (TextUtils.isEmpty(phoneNumber)) {
+            phoneNumberEditText.setError("Phone number cannot be empty");
+            error = false;
+        }
+
+        if (TextUtils.isEmpty(streetAddress)) {
+            streetAddressEditText.setError("Street address cannot be empty");
+            error = false;
+        }
+
+        if (TextUtils.isEmpty(apartmentNumber)) {
+            apartmentNumberEditText.setError("Apartment number cannot be empty");
+            error = false;
+        }
+
+        if (TextUtils.isEmpty(city)) {
+            cityEditText.setError("City cannot be empty");
+            error = false;
+        }
+
+        if (TextUtils.isEmpty(postalCode)) {
+            postalCodeEditText.setError("Postal Code cannot be empty");
+            error = false;
+        }
+
+        return error;
     }
 }
