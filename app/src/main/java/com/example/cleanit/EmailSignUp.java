7c3688185a41850ef668f5232330a7e442aa0b96
Adel Araji <adel_araji@hotmail.com>
2020-04-03 14:44:56 -0400

    Added error checking for email, facebook and google signup

--- app/src/main/java/com/example/cleanit/EmailSignUp.java
+++ app/src/main/java/com/example/cleanit/EmailSignUp.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 
 public class EmailSignUp extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +20,34 @@ public class EmailSignUp extends AppCompatActivity {
     }
 
     public void goToAdditionalSignUpPage(View view) {
-        Intent intent = new Intent(this, AdditionalSignUpStage.class);
-        startActivity(intent);
+
+        //Get email and password before clickinng the button
+        emailEditText = findViewById(R.id.emailInputText);
+        passwordEditText = findViewById(R.id.passwordInputText);
+
+        if (validForm()) {
+            Intent intent = new Intent(this, AdditionalSignUpStage.class);
+            startActivity(intent);
+        }
+    }
+
+    public boolean validForm() {
+        boolean error = true;
+
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+
+        if (TextUtils.isEmpty(email)) {
+            emailEditText.setError("Email cannot be empty");
+            error = false;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            passwordEditText.setError("Password cannot be empty");
+            error = false;
+        }
+
+        return error;
     }
 }
--- app/src/main/java/com/example/cleanit/GoogleSignUp.java
+++ app/src/main/java/com/example/cleanit/GoogleSignUp.java
@@ -4,18 +4,53 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 
 public class GoogleSignUp extends AppCompatActivity {
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_sign_up);
+
     }
 
     public void goToAdditionalSignUpPage(View view) {
-        Intent intent = new Intent(this, AdditionalSignUpStage.class);
-        startActivity(intent);
+
+
+        //Get email and password before clickinng the button
+        emailEditText = findViewById(R.id.emailInputText);
+        passwordEditText = findViewById(R.id.passwordInputText);
+
+        if (validForm()) {
+            Intent intent = new Intent(this, AdditionalSignUpStage.class);
+            startActivity(intent);
+        }
+
+    }
+
+    public boolean validForm() {
+        boolean error = true;
+
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+
+        if (TextUtils.isEmpty(email)) {
+            emailEditText.setError("Email cannot be empty");
+            error = false;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            passwordEditText.setError("Password cannot be empty");
+            error = false;
+        }
+
+        return error;
     }
 }
